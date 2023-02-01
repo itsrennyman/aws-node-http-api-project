1dc02db1699f97ff2214d12076913eb84d147b76
Renato Pozzi <renato.pozzi.dev@gmail.com>
2023-02-01 17:20:22 +0100

    feat: changed to post & module

--- handler.js
+++ handler.js
@@ -1,15 +1,24 @@
 "use strict";
 
 module.exports.hello = async (event) => {
+  const result = await createPDFDocument();
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Champagne Per Nessuno!",
+        ...result,
         input: event,
       },
       null,
       2
     ),
+  };
+};
+
+const createPDFDocument = async () => {
+  return {
+    status: "success",
+    message: "Champagne Per Nessuno!",
   };
 };
