7b7092d99ac060ebec52640aacb93b5036016a37
Renato Pozzi <renato.pozzi.dev@gmail.com>
2023-02-01 17:38:16 +0100

    feat: added content-type

--- handler.js
+++ handler.js
@@ -6,6 +6,9 @@ module.exports.hello = async (event) => {
 
   return {
     statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify({ ...result, data }),
   };
 };
